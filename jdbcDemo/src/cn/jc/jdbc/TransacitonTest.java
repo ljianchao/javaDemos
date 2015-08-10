f8acd987bbc2746c9aad2a37e86ba18ef5abbd43
ljianchao <ljianchao@163.com>
2015-08-10 15:04:14 +0800

    测试数据库的隔离级别

--- jdbcDemo/src/cn/jc/jdbc/TransacitonTest.java
+++ jdbcDemo/src/cn/jc/jdbc/TransacitonTest.java
@@ -2,12 +2,55 @@ package cn.jc.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.Test;
 
 public class TransacitonTest {
 
+	/**
+	 * 测试事务的隔离级别
+	 */
+	@Test
+	public void testTransactionIsolationUpdate() {
+		Connection connection = null;
+		
+		try {
+			connection = JDBCTools.getConnection();
+			connection.setAutoCommit(false);
+			
+			String sql = "UPDATE users SET balace = " +
+					"balace - 500 WHERE id = 1";
+			update(connection,sql);
+			
+			connection.commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			//回滚事务
+			try {				
+				connection.rollback();
+			} catch (SQLException e1) { 
+				e1.printStackTrace();
+			}
+		} finally{
+			JDBCTools.release(null, connection);
+		}
+	}
+	
+	/**
+	 * 测试事务的隔离级别
+	 */
+	@Test
+	public void testTransactionIsolationRead() {
+		String sql = "SELECT balace FROM users WHERE id = 1";
+		
+		Float balace = getForValue(sql);
+		System.out.println(balace);
+	}
+	
 	/**
 	 * 关于事务：
 	 * 1. 如果多个操作，每个操作使用的是自己的单独的连接，则无法保证事务
@@ -87,4 +130,38 @@ public class TransacitonTest {
 			JDBCTools.release(null,  preparedStatement, null);
 		}
 	}
+	
+	//返回某条记录的某个字段或一个统计的值
+		public <E> E getForValue(String sql, Object... args){		
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+			ResultSet resultSet = null;
+			
+			try {
+				//1. 获取connection
+				connection = JDBCTools.getConnection();
+				// 设置事务的隔离级别
+				connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+				
+				//2. 获取PreparedStatement
+				preparedStatement = connection.prepareStatement(sql);
+				//3. 填充占位符
+				for (int i = 0; i < args.length; i++) {
+					preparedStatement.setObject(i + 1, args[i]);
+				}
+				//4. 进行查询，得到ResultSet
+				resultSet = preparedStatement.executeQuery();
+				
+				//5. 若ResultSet中有记录，准备一个Map<String,Object>
+				if (resultSet.next()) {
+					return (E)resultSet.getObject(1);
+				}			
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally{
+				JDBCTools.release(resultSet, preparedStatement, connection);			
+			}
+			
+			return null;
+		}
 }
